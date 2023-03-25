2c7c75cd013411d33e0d6f8f49e8cb9eb9277bd5
DontWorryYouCanMerge <fiveminutes001@gmail.com>
2023-03-25 07:28:29 +0300

    build

--- models/server.js
+++ models/server.js
@@ -22,9 +22,9 @@ class Server {
 
   // Bind controllers to routes
   routes() {
-    this.app.use(this.paths.default, require("../routes/auth"));
-    this.app.use(this.paths.auth, require("../routes/auth"));
-    this.app.use(this.paths.homepage, require("../routes/homepage"));
+    // this.app.use(this.paths.default, require("../routes/auth"));
+    // this.app.use(this.paths.auth, require("../routes/auth"));
+    // this.app.use(this.paths.homepage, require("../routes/homepage"));
     this.app.use(express.static(path.join(__dirname, 'build')));
     this.app.get('/*', function (req, res) {
       res.sendFile(path.join(__dirname, 'build', 'index.html'));
