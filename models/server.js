0702c4c8118642f1e9417bd8bc19f10ee8f7bf6c
DontWorryYouCanMerge <fiveminutes001@gmail.com>
2023-03-25 07:21:38 +0300

    still working on build to work

--- models/server.js
+++ models/server.js
@@ -27,8 +27,8 @@ class Server {
     this.app.use(this.paths.homepage, require("../routes/homepage"));
   }
 
-  app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', function (req, res) {
+  this.app.use(express.static(path.join(__dirname, 'build')));
+this.app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
   listen() {
